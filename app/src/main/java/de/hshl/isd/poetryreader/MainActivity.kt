8049bde08617286f28dd7eb81c1c95f578ce9e46
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-12-05 11:22:42 +0100

    Refactoring

--- app/src/main/java/de/hshl/isd/poetryreader/MainActivity.kt
+++ app/src/main/java/de/hshl/isd/poetryreader/MainActivity.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import de.hshl.isd.poetryreader.domain.PoetryReader
 import de.hshl.isd.poetryreader.ui.theme.PoetryReaderTheme
 import kotlinx.coroutines.launch
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainContent() {
     val tag = "MainContent"
-    var resultText by remember { mutableStateOf("") }
+    var resultText by remember { mutableStateOf("I want to sleep\r\nSwat the flies\r\nSoftly, please.\r\n\r\n-- Masaoka Shiki (1867-1902)") }
     val poetryReader = PoetryReader()
     val scope = rememberCoroutineScope()
 
@@ -41,20 +50,27 @@ fun MainContent() {
         Log.e(tag, error.localizedMessage!!)
     }
 
-    Column {
-        Text("Poetry Reader")
-        Button(onClick = {
-            scope.launch {
-                kotlin.runCatching {
-                    poetryReader.giveMeSomePoetry()
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    , verticalArrangement = Arrangement.SpaceAround) {
+        Text("Poetry Reader", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+//        Column(vertical = Alignment.Bottom) {
+            Button(onClick = {
+                scope.launch {
+                    kotlin.runCatching {
+                        poetryReader.giveMeSomePoetry()
+                    }
+                        .onSuccess(::success)
+                        .onFailure(::failure)
                 }
-                    .onSuccess(::success)
-                    .onFailure(::failure)
+            }) {
+                Text("Give me some poetry")
             }
-        }) {
-            Text("Give me some poetry")
-        }
-        Text(resultText)
+            Text(resultText)
+        //}
+
     }
 }
 
